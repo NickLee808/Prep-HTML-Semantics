9be5343eb7f0e4f4543ee2ec16f3763f31628ffe
Nick Lee <nicklee808@gmail.com>
2016-10-19 17:59:29 -1000

    ahnuld 1.0

---
+++ MY FIRST WEBSITE/JS/apps.js
@@ -0,0 +1,31 @@
+var titles = ["I'LL BE BACK", "HASTA LA VISTA... BABY", "DASHER, DANCER, PRANCER, VIXEN, COMET, CUPID, DONNER, BLITZEN", "WHO IS YOUR DADDY AND WHAT DOES HE DO", "GET TO DA CHOPPA", "WHO TOLD YOU YOU COULD EAT MY COOKIES?", "IT'S TURBO TIME", "CRUSH YOUR ENEMIES, SEE THEM DRIVEN BEFORE YOU, AND TO HEAR THE LAMENTATIONS OF THEIR WOMEN", "I'M THE PARTY POOPER", "IF IT BLEEDS, WE CAN KILL IT", "LET OFF SOME STEAM, BENNETT", "IT'S NOT A TUMOR", "I HOPE YOU LEAVE ENOUGH ROOM FOR MY FIST, BECAUSE I'M GOING TO RAM IT INTO YOUR STOMACH", "ARE YOU SARAH CONNOR?", "SEE YOU AT THE PARTY, RICHTER", "YOU ARE ONE UGLY MOTHERFUCKER", "REMEMBER WHEN I PROMISED TO KILL YOU LAST? I LIED", "I NEED YOUR CLOTHES, YOUR BOOTS, AND YOUR MOTORCYCLE", "CHILL OUT, DICKWAD", "DON'T DISTURB MY FRIEND, HE'S DEAD TIRED", "YOU LACK DISCIPLINE", "YOU'RE NOT SENDING ME TO THE COOLER", "COME WITH ME IF YOU WANT TO LIVE"];
+
+var randTitles = titles[Math.floor(Math.random() * titles.length)];
+
+
+
+var fruits1 = ["Bananas", "Oranges", "Apples", "Kiwis", "Tomatoes", "Strawberries"]; 
+var fruits2 = ["Blueberries", "Raspberries", "Watermelons", "Lemons", "Limes", "Cucumbers", "Apricots"];
+var fruits3 = ["Avocados", "Boysenberries", "Cherries", "Coconuts", "Cranberries", "Dates", "Dragonfruits"];
+var fruits4 = ["Gooseberries", "Grapes", "Guavas", "Lychees", "Mangoes", "Cantaloupes", "Honeydews"];
+var fruits5 = ["Nectarines", "Tangerines", "Papayas", "Peaches", "Pears", "Plums", "Pomegranates",];
+
+var randFruits1 = fruits1[Math.floor(Math.random() * fruits1.length)];
+var randFruits2 = fruits2[Math.floor(Math.random() * fruits2.length)];
+var randFruits3 = fruits3[Math.floor(Math.random() * fruits3.length)];
+var randFruits4 = fruits4[Math.floor(Math.random() * fruits4.length)];
+var randFruits5 = fruits5[Math.floor(Math.random() * fruits5.length)];
+
+
+
+var vegetables1 = ["Arugulas", "Beets", "Bok Choy", "Broccoli", "Brussels Sprouts", "Cabbages", "Celery", "Collard Greens", "Corn", "Kale"];
+var vegetables2 = ["Watercress", "Lettuce", "Peas", "Spinach", "Turnips", "Carrots", "Artichokes", "Capers", "Cauliflowers", "Squash", "Azuki Beans"];
+var vegetables3 = ["Chickpeas", "Fava Beans", "Garbanzo Beans", "Green Beans", "Lentils", "Lima Beans", "Mung Beans", "Okra", "Peanuts", "Snap Peas", "Snow Peas"];
+var vegetables4 = ["Soybeans", "Asparagus", "Chives", "Garlic", "Green Onions", "Onions", "Lemongrass", "Lotus Roots", "Ginger", "Shallots", "Leeks"];
+var vegetables5 = ["Bamboo", "Daikon", "Horseradish", "Parsnips", "Potatoes", "Radishes", "Sweet Potatoes", "Taro", "Turnips", "Chestnuts", "Seaweed"];
+
+var randVegetables1 = vegetables1[Math.floor(Math.random() * vegetables1.length)];
+var randVegetables2 = vegetables2[Math.floor(Math.random() * vegetables2.length)];
+var randVegetables3 = vegetables3[Math.floor(Math.random() * vegetables3.length)];
+var randVegetables4 = vegetables4[Math.floor(Math.random() * vegetables4.length)];
+var randVegetables5 = vegetables5[Math.floor(Math.random() * vegetables5.length)];
